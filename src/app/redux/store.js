a3fab58d8044d45ed9bf46d740fc5f0b7e486c96
SergeyLipko <lipko.js@gmail.com>
2017-05-14 19:41:29 +0300

    Update store.js

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-// import rootReducer from './reducers';
 import rootReducer from './modules/rootReducer';
 
 const handleMiddlewares = withLogger => (...middleware) => {
